8246c5051f055aa4ce0efc37200a4305192a24de
Bayram Erdem <bayram9erdem@gmail.com>
2026-08-02 18:13:19 +0200

    Add few more tests for viewing applications

--- test_app.py
+++ test_app.py
@@ -10,3 +10,22 @@ def test_home():
     assert response.get_json() == {
         "message": "Job Application Tracker API"
     }
+
+def test_get_all_applications():
+    client = app.test_client()
+
+    response = client.get("/applications")
+
+    assert response.status_code == 200
+    assert isinstance(response.get_json(), list)
+
+
+def test_unknown_application():
+    client = app.test_client()
+
+    response = client.get("/applications/999")
+
+    assert response.status_code == 404
+    assert response.get_json() == {
+        "error": "Application not found"
+    }
