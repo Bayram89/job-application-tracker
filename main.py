d9ac5fad3142df1acc559141604c913b52d263e3
Bayram Erdem <bayram9erdem@gmail.com>
2026-08-02 12:26:32 +0200

    Add simple application menu

---
+++ main.py
@@ -0,0 +1,16 @@
+print("Job Application Tracker")
+print("1. Add an application")
+print("2. Show all applications")
+print("3. Exit")
+choice = input("Choose an option: ")
+if choice == "1":
+    print("Add an application")
+
+elif choice == "2":
+    print("Show all applications")
+
+elif choice == "3":
+    print("Goodbye")
+
+else:
+    print("That is not a valid option")
