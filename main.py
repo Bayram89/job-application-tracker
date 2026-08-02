f1a9a1b7397724313a23c87d6bd892fb8e0b8c92
Bayram Erdem <bayram9erdem@gmail.com>
2026-08-02 13:17:58 +0200

    Add application list and repeating menu

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
-print("Job Application Tracker")
-print("1. Add an application")
-print("2. Show all applications")
-print("3. Exit")
-choice = input("Choose an option: ")
+applications = []
 
-if choice == "1":
-    company = input("Company name: ")
-    position = input("Position: ")
-    status = input("Status: ")
+while True:
+    print()
+    print("Job Application Tracker")
+    print("1. Add an application")
+    print("2. Show all applications")
+    print("3. Exit")
 
-    print("Application added")
-    print("Company:", company)
-    print("Position:", position)
-    print("Status:", status)
+    choice = input("Choose an option: ")
 
-elif choice == "2":
-    print("Show all applications")
+    if choice == "1":
+        company = input("Company name: ")
+        position = input("Position: ")
+        status = input("Status: ")
 
-elif choice == "3":
-    print("Goodbye")
+        application = {
+            "company": company,
+            "position": position,
+            "status": status
+        }
 
-else:
-    print("That is not a valid option")
+        applications.append(application)
+
+        print("Application added")
+        print("Number of applications:", len(applications))
+
+    elif choice == "2":
+        print("Show all applications")
+
+    elif choice == "3":
+        print("Goodbye")
+        break
+
+    else:
+        print("That is not a valid option")
