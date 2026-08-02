6dbce745b4c2320c387dde6a3851a50d7d153204
Bayram Erdem <bayram9erdem@gmail.com>
2026-08-02 12:36:16 +0200

    Gather application details

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ print("1. Add an application")
 print("2. Show all applications")
 print("3. Exit")
 choice = input("Choose an option: ")
+
 if choice == "1":
-    print("Add an application")
+    company = input("Company name: ")
+    position = input("Position: ")
+    status = input("Status: ")
+
+    print("Application added")
+    print("Company:", company)
+    print("Position:", position)
+    print("Status:", status)
 
 elif choice == "2":
     print("Show all applications")
